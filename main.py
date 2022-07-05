b958d6455739e170ee85fd668a58a4098f71674a
jharris19 <hydro.yoshi@gmail.com>
2022-07-04 19:40:37 -0500

    Flask implementation

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 import os
+from flask import Flask
+
+
+app = Flask(__name__)
 
 
 def hello(name):
-    print(f"hello, {name}!")
+    return f"hello, {name}!"
 
 
-if __name__ == '__main__':
-    hello(os.getenv('HELLO_NAME', 'pyCharm'))
+@app.route("/")
+def hello_world():
+    return f"<p>{hello(os.getenv('HELLO_NAME', 'pyCharm'))}</p>"
